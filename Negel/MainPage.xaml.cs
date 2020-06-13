5cb8be7ec3869218fc5c2087783f60086efd8d7a
Aswin P G <aswinpg4444@outlook.com>
2020-06-13 21:09:22 +0530

    Add project files.

---
+++ Negel/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Negel
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MainCollectionView.ItemsSource = new ObservableCollection<string>()
+            {
+                "SEPM",
+                "PDD",
+                "COA",
+                "Maths",
+                "OOP",
+                "C#",
+                "Java",
+                "Python"
+
+
+            };
+        }
+    }
+}
